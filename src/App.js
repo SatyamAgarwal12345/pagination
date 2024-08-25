b15a5d3de9039ca72532df78946533bda7fc55e7
SatyamAgarwal12345 <satyamagarwal535@gmail.com>
2024-08-25 16:37:27 +0530

    api call and cards css map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import "./App.css";
+import { useState, useEffect } from "react";
 
-import './App.css';
-
-function App() {
+export default function App() {
+  const [products, setproducts] = useState([]);
+  const fetchProducts = async () => {
+    const res = await fetch(`https://dummyjson.com/products?limit=100`);
+    const data = await res.json();
+    setproducts(data.products);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
+  
   return (
-    <div className="App">
-     hii
+    <div>
+      <div className="products">
+        {products.map((prod) => (
+          <span className="product__single">
+            <img src={prod.images[0]}></img>
+            <span className="text">{prod.title}</span>
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
